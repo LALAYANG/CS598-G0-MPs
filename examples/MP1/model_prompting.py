2211ce9040387a0fceec5a5bda5fe96961156ddb
LALAYANG <yangc9@illinois.edu>
2024-08-21 12:51:49 -0500

    update examples/MP1/model_prompting.py

--- examples/MP1/model_prompting.py
+++ examples/MP1/model_prompting.py
@@ -5,7 +5,7 @@ from transformers import AutoTokenizer, AutoModelForCausalLM, BitsAndBytesConfig
 
 #####################################################
 # Please finish all TODOs in this file for MP1;
-# do not change other functions/formatting.
+# do not change other code/formatting.
 #####################################################
 
 def save_file(content, file_path):
@@ -16,38 +16,20 @@ def prompt_model(dataset, model_name = "deepseek-ai/deepseek-coder-6.7b-base", q
     print(f"Working with {model_name} quantization {quantization}...")
     
     # TODO: download the model
-    tokenizer = AutoTokenizer.from_pretrained(model_name, trust_remote_code=True)
+    
   
     if quantization:
         # TODO: load the model with quantization
-        model = AutoModelForCausalLM.from_pretrained(
-                pretrained_model_name_or_path=model_name,
-                device_map='auto',
-                torch_dtype=torch.bfloat16,
-                temperature=0,
-                quantization_config=BitsAndBytesConfig(
-                    load_in_4bit=True,
-                    bnb_4bit_compute_dtype=torch.bfloat16,
-                    bnb_4bit_use_double_quant=True,
-                    bnb_4bit_quant_type='nf4'
-                ),
-            )
+        
     else:
         # TODO: load the model without quantization
-        model = AutoModelForCausalLM.from_pretrained(
-                pretrained_model_name_or_path=model_name,
-                device_map='auto',
-                torch_dtype=torch.bfloat16,
-                temperature=0,
-                )
+        
 
     results = []
     for case in dataset:
         prompt = case['prompt']
         # TODO: prompt the model and get the response
-        inputs = tokenizer(prompt, return_tensors="pt").to(model.device)
-        outputs = model.generate(**inputs, max_length=500)
-        response = tokenizer.decode(outputs[0], skip_special_tokens=True)
+        
         print(f"Task_ID {case['task_id']}:\nPrompt:\n{prompt}\nResponse:\n{response}")
         results.append(dict(task_id=case["task_id"], completion=response))
     return results
